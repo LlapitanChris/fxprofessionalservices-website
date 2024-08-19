44eab6a6b6b7ee704fe0daff21738e20822e8db0
Christine Fradejas <christine@vidaura.com.au>
2024-08-19 21:44:29 +0800

    feat: favicon

--- app/(overview)/team/[devName]/layout.js
+++ app/(overview)/team/[devName]/layout.js
@@ -14,6 +14,10 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
           <meta name="theme-color" content="#211F53" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicon_io/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon_io/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon_io/favicon-16x16.png"/>
+          <link rel="manifest" href="/favicon_io/site.webmanifest"/>
       </head>
       <body className={inter.className}>
         <SplashScreen />
